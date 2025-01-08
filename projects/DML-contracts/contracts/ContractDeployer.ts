6618f13cfa7c3224ccb99dd118a5256dcf2fcae5
acez0z <anupaxtream@gmail.com>
2025-01-08 22:37:46 +0530

    feat: deployer config

--- projects/DML-contracts/contracts/ContractDeployer.ts
+++ projects/DML-contracts/contracts/ContractDeployer.ts
@@ -1,3 +1,21 @@
-export const contractDeployer = async (ipfsHash: string) => {
-  console.log(ipfsHash);
+import { AlgorandClient } from '@algorandfoundation/algokit-utils';
+import { DmlChainClient } from './clients/DMLChainClient';
+
+export const contractDeployer = async (ipfsHash: string, modelParameters: string) => {
+  console.log(ipfsHash + modelParameters);
+
+  const algorand = AlgorandClient.defaultLocalNet();
+  algorand.setDefaultValidityWindow(1000);
+
+  const dispenser = await algorand.account.localNetDispenser();
+
+  const acct = algorand.account.random();
+
+  algorand.send.payment({
+    sender: dispenser.addr,
+    receiver: acct.account.addr,
+    amount: (4).algo(),
+  });
+
+  const app_client = DmlChainClient();
 };
